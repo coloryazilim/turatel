d4aae11ed29cb09dbd93f1add07de80e7e122ec9
Yasar icli <hello@yasaricli.com>
2017-02-24 11:20:15 +0300

    change class status and add callback success send sms.

--- turatel.js
+++ turatel.js
@@ -1,5 +1,6 @@
 import request from 'request';
 import { jsonxml } from './lib/jsontoxml';
+import { STATUS } from './status';
 
 export class Turatel {
   constructor(auth, options) {
@@ -19,39 +20,9 @@ export class Turatel {
       sendSms: true,
       debug: false
     }, options);
-
-    this.ERRORS = {
-      '1' : 'SMS başarıyla gönderildi.',
-      '2'	: 'Bir alıcı girmelisiniz.',
-      '3'	: 'Alıcı dizisinde hatalı eleman var.',
-      '4'	: 'Bir SMS metni girmelisiniz.',
-      '5'	: 'SMS gönderilecek tarih formatını hatalı girdiniz. (Format: Y-m-d H:i)',
-      '6'	: 'Ayar dosyasında varsayılan SMS bağlığı boş olamaz.',
-      '7'	: 'Birden fazla alıcıya birden fazla farklı mesaj tanımlanmış. Ancak alıcı sayısıyla mesaj sayısı uyuşmuyor.',
-      '20': 'Geçersiz bir SMS hizmet sağlayıcı girdiniz.',
-      '21': 'Seçtiğiniz SMS hizmet sağlayıcı için API adresi bulunmuyor.',
-      '22': 'Geçersiz bir SMS başlığı(originator - senderID) girdiniz.',
-      '900': 'İstek işlenirken teknik bir problem meydana geldi.',
-      '901': 'SMS gönderilemedi. (Hizmet sağlayıcıdan dönen hata mesajı yakalanamadı)',
-      '902': 'SMS gönderilemedi. (Parametre kontrolünde hata mevcut @todo)',
-      '00': 'Sistem Hatası.',
-      '20': 'Tanımsız Hata (XML formatını kontrol ediniz veya TURATEL’den destek alınız).',
-      '21': 'Hatalı XML Formatı (\n - carriage return – newline vb içeriyor olabilir).',
-      '22': 'Kullanıcı Aktif Değil.',
-      '23': 'Kullanıcı Zaman Aşımında.',
-      '01': 'Kullanıcı adı ya da şifre hatalı.',
-      '02': 'Kredisi yeterli değil.',
-      '03': 'Geçersiz içerik.',
-      '04': 'Bilinmeyen SMS tipi.',
-      '05': 'Hatalı gönderen ismi.',
-      '06': 'Mesaj metni ya da Alıcı bilgisi girilmemiş.',
-      '07': 'İçerik uzun fakat Concat özelliği ayarlanmadığından mesaj birleştirilemiyor.',
-      '08': 'Kullanıcının mesaj göndereceği gateway tanımlı değil ya da şu anda çalışmıyor.',
-      '09': 'Yanlış tarih formatı.Tarih ddMMyyyyhhmm formatında olmalıdır.',
-    }
   }
 
-  instance(options) {
+  toXml(options) {
     return jsonxml({
       MainmsgBody: {
         ...this.AUTH,
@@ -60,36 +31,33 @@ export class Turatel {
     }, this.OPTIONS);
   }
 
-  getError(code) {
-    return this.ERRORS[code];
-  }
-
-  send(Mesgbody, NumbersOut) {
+  send(Mesgbody, NumbersOut, callback) {
     let Numbers = NumbersOut;
 
     if (_.isArray(NumbersOut)) {
       Numbers = NumbersOut.join(',');
     }
 
-    const instance = this.instance({ Mesgbody, Numbers });
+    // export xml.
+    const xmlData = this.toXml({ Mesgbody, Numbers });
 
     if (this.OPTIONS.debug) {
-      console.log(instance)
+      console.log(xmlData);
     }
 
     if (this.OPTIONS.sendSms) {
       request.post({
         url: this.SEND_URL,
-        body : instance,
+        body : xmlData,
         headers: { 'Content-Type': 'text/xml' }
       }, (error, response, body) => {
-        if (!error && response.statusCode == 200) {
-          const err = this.getError(body);
-          if (err) {
-            return console.log(err);
+        if (_.isEqual(response.statusCode, 200)) {
+
+          if (this.OPTIONS.debug) {
+            console.log(body, '- Mesaj başarılı şekilde gönderildi.');
           }
 
-          console.log(body, '- Mesaj başarılı şekilde gönderildi.');
+          return callback(body, STATUS[body]);
         }
       });
     }
